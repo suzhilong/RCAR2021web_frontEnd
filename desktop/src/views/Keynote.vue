bfeca2a9c9a7d2ad9d0cfb5b8445d25e76c4871e
SOUSIC <517048082@qq.com>
2021-06-12 13:49:56 +0800

    add keynote speaker3 4

--- desktop/src/views/Keynote.vue
+++ desktop/src/views/Keynote.vue
@@ -63,22 +63,22 @@
           </strong>
       </h3>
       <br>
-      <!-- <img src="./../../../src/images/SONGZhan.png" class="card-im-left" width="175" height="222"> -->
+      <img src="./../../../src/images/3HuihuanQian.png" class="card-im-left" width="175" height="222">
       <br>
       <h4>
-        <strong> Prof. </strong>
+        <strong> Prof. Huihuan Qian, Chinese University of Hong Kong, China</strong>
       </h4>
       <h4>
-        <strong>Title: </strong>
+        <strong>Title: Intelligent Sailing Robots Towards Marine Surface Exploration</strong>
       </h4>
       <h5>
       <p style="text-align:justify">
         <strong>Abstract:</strong>
-        
+        To meet the continuously increasing need in marine surface exploration, intelligent robots with long-range capability is crucial to reduce risk to human participants and increase efficiency. This talk will present the effort in intelligent sailing robots at different scales. Starting from small-size sailboats in controlled wind field, we model the propelling thrust by force polar diagram (FPD) and control the robot to enhance its mobility. To explore for long-range capability, we extend research effort to a large-size sailboat OceanVoy. Hybrid scheme combining Pseudo-Spectral (PS) optimal control on rudder and Extreme Seeking Control (ESC) on sail is developed to improve the performance. Furthermore, we enhance V-stability based control and reduce the control frequency to save energy. Around 1200km experiment is accumulated.
       </p>
       <p style="text-align:justify">
         <strong>Biography:</strong>
-        
+        Prof. Qian received his Ph.D. in Automation and Computer-Aided Engineering from the Chinese University of Hong Kong, and thereafter worked as Research Assistant Professor there. He is currently Assistant Professor at the School of Science and Engineering in the Chinese University of Hong Kong, Shenzhen. He also serves as Vice President in Shenzhen Institute of Artificial Intelligence and Robotics for Society (AIRS). His research interest include design and control of robots and intelligent system, and currently focus on marine surface robotics. He has received the Best Transaction Paper Award from IEEE/ASME Transactions on Mechatronics in 2011 with his colleagues, based on the research of steering interface for omni-directional vehicles. With his postgraduate/undergraduate students, he has received a number awards recently, including IROS2020 Best Application Paper Finalist, ROBIO2019 Best Paper Finalist, RCAR2019 Best Paper Finalist, WCICA2018 Best Student Paper Finalist. He serves as an Associate Editor in IEEE International Conference on Robotics and Automation (ICRA) and an Area Chair in Robotics: Science and Systems (RSS). 
         </p>
       </h5>
 
@@ -89,26 +89,24 @@
           </strong>
       </h3>
       <br>
-      <!-- <img src="./../../../src/images/LinWang.jpg" class="card-im-left" width="175" height="222"> -->
+      <img src="./../../../src/images/4MingmingZhang.png" class="card-im-left" width="175" height="222">
       <br>
       <h4>
-        <strong> Prof. </strong>
+        <strong> Prof. Mingming Zhang, Southern University of Science and Technology, China</strong>
       </h4>
       <h4>
-        <strong>Title: </strong>
+        <strong>Title: Task-oriented rehabilitation techniques with robot-assisted information feedback</strong>
       </h4>
       <h5>
       <p style="text-align:justify">
         <strong>Abstract:</strong>
-        
+        Individuals with physical disabilities are often restricted to perform activities of daily life (ADLs). While efficacy of bilateral training has been demonstrated in improving physical coordination of human limbs, few robots have been developed in simulating people’s ADLs integrated with task-specific force field control. This study sought to develop a bilateral robot for better task rendering of general ADLs (gADLs), where gADL-consistent workspace is achieved by setting linear motors in series, and haptic rendering of multiple bimanual tasks (coupled, uncoupled and semi-coupled) is enabled by regulating force fields between robotic handles. Experiments were conducted with human users, and our results present a viable method of a single robotic system in simulating multiple physically bimanual tasks. The proposed robotic system is expected to be serving as a coordination training device, and its clinical efficacy will be also investigated. In this task, other relevant robot-assisted rehabilitation techniques will also be briefly introduced.
       </p>
       <p style="text-align:justify">
         <strong>Biography:</strong>
-        
+        Mingming Zhang, IEEE Senior Member, Principal Investigator of Brain-Robot Rehabilitation Technology Lab, Department of Biomedical Engineering, Southern University of Science and Technology (Shenzhen, China). His main research interests include tactile sensing and feedback technology, intelligent control and human-computer interaction, wearable exoskeleton rehabilitation robots, and EMG/EEG based pattern recognition algorithms. He received the Ph.D. degree in Mechanical Engineering from the University of Auckland, New Zealand, in 2015. From 2015 to 2016, he worked as a postdoc research fellow in the Department of Mechanical Engineering at the University of Auckland. From early 2017 to 2018, he worked at CASTIOT LIMITED (Auckland) as a vice president in developing lower limb rehabilitation robotics. He was also employed as a visiting research fellow at the University of Auckland. He has authored over 50 academic papers published on peer-reviewed international journals in the fields of robotics, intelligent control, neural rehabilitation, and biomechanics, including IEEE Transactions on Industrial Electronics, IEEE Transactions on Biomedical Engineering, Journal of NeuroEngineering and Rehabilitation, Journal of Biomechanics, etc. Over 30 patents have been applied or authorized as well. He also authored an academic book published by Institution of Engineering and Technology.
         </p>
       </h5>
-
-
   </div>
 </template>
 
